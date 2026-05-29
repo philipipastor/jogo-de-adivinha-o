a2308aaeef13e9d63e3f2e6ee85e8ee3f907dad1
Philipi Pastor <philipipastor123@gmail.com>
2026-05-29 15:12:35 -0300

    feat: adicionando componentes ao App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Header } from "./components/Header/index"
+import { Tip } from "./components/Tip/index"
+
+import styles from "./app.module.css"
+
 export default function App(){
+  function handleRestart(){
+    alert("Reiniciar o jogo!")
+  }
+
   return (
-    <div>"
-      <h1>React + Vite + TypeScript</h1>
+    <div className={styles.container}>
+      <main>
+        <Header current={5} max={10} onRestart={handleRestart}/>
+        
+        <Tip textoDica={"teste"}/>
+      </main>
     </div>
   )
 }
