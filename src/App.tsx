176f1d5a9f55cf06403aabb8247b6c41ae4d1bb9
Philipi Pastor <philipipastor123@gmail.com>
2026-05-31 18:04:39 -0300

    feat: adicionando componentes ao app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Header } from "./components/Header/index"
 import { Tip } from "./components/Tip/index"
+import { Palpite } from "./components/Palpite"
+import { Letter } from "./components/Letter"
 
 import styles from "./app.module.css"
 
@@ -14,6 +16,18 @@ export default function App(){
         <Header current={5} max={10} onRestart={handleRestart}/>
         
         <Tip textoDica={"teste"}/>
+        
+        <div className={styles.word}>
+          <Letter value="R"/>
+          <Letter value="E"/>
+          <Letter value="A"/>
+          <Letter value="A"/>
+          <Letter value="C"/>
+          <Letter value="T"/>
+        </div>
+
+        <Palpite />
+
       </main>
     </div>
   )
