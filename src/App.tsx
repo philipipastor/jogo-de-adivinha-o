a200c179ab7a8c648c937804f1a18e1998045400
Philipi Pastor <philipipastor123@gmail.com>
2026-06-01 17:47:50 -0300

    feat: adicionando funcionalidades aos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,63 @@ import { Header } from "./components/Header/index"
 import { Tip } from "./components/Tip/index"
 import { Palpite } from "./components/Palpite"
 import { Letter } from "./components/Letter"
-import { LetterUsed } from "./components/LetterUsed"
+import { LetterUsed, type LetterUsedProps } from "./components/LetterUsed"
+
+import {WORDS, type Challenge } from "./utils/words"
+import { useEffect, useState } from "react"
 
 import styles from "./app.module.css"
 
 export default function App(){
+
+  const [attempts, setattempts] = useState(0)
+  const [letterUsed, setletterUsed] = useState<LetterUsedProps[]>([])
+  const [letter, setLetter] = useState("")
+  const [challenge, setChallenge] = useState<Challenge | null>(null)
+
   function handleRestart(){
     alert("Reiniciar o jogo!")
+  }
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length)
+    const randomWord = WORDS[index]
+
+    setChallenge(randomWord)
+
+    setattempts(0) 
+    setLetter("")
+
+  }
+
+  useEffect(() => {
+    startGame()
+  }, [])
+
+  if(!challenge){
+    return
   }
 
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestart}/>
+        <Header current={attempts} max={10} onRestart={handleRestart}/>
         
         <Tip textoDica={"teste"}/>
         
         <div className={styles.word}>
-          <Letter value="R"/>
-          <Letter value="E"/>
-          <Letter value="A"/>
-          <Letter value="A"/>
-          <Letter value="C"/>
-          <Letter value="T"/>
+          {
+            challenge.word.split("").map(() => (
+              <Letter value="" />
+            ))
+          }
+
         </div>
 
         <Palpite />
 
         <div>
-          <LetterUsed />
+          <LetterUsed data={letterUsed}/>
         </div>
       </main>
     </div>
