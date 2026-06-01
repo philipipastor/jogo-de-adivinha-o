ceab6f23f56fd99b5504a22995b6b5b9f2839aa8
Philipi Pastor <philipipastor123@gmail.com>
2026-06-01 17:47:24 -0300

    feat: listando letras utilizadas

--- src/components/LetterUsed/index.tsx
+++ src/components/LetterUsed/index.tsx
@@ -1,16 +1,26 @@
 import { Letter } from "../Letter/index"
 import styles from "./style.module.css"
 
+export type LetterUsedProps = {
+    value: string
+    correct: boolean
+}
 
-export function LetterUsed(){
+type Props = {
+    data: LetterUsedProps[] 
+}
+
+export function LetterUsed({data}: Props){
     return (
         <div className = {styles.letterUsed}>
             <h5>Letras utilizadas</h5>
 
             <div>
-                <span><Letter value="x" size="small" color="wrong"/></span>
-                <span><Letter value="x" size="small" color="correct"/></span>
-
+                {
+                    data.map(({value, correct}) => (
+                        <Letter value={value} size="small" color= {correct ? "correct" : "wrong"}/>
+                    ))
+                }
             </div>
         </div>
     )
