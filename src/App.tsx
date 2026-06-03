c4ee6ff051a08999949bbd3f7a2865da3b5e3ec6
Philipi Pastor <philipipastor123@gmail.com>
2026-06-03 15:34:50 -0300

    feat: adiciona funcionalidade aos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header/index"
 import { Tip } from "./components/Tip/index"
-import { Palpite } from "./components/Palpite"
+import { Input } from "./components/Input/index"
+import { Button} from "./components/Button/index"
 import { Letter } from "./components/Letter"
 import { LetterUsed, type LetterUsedProps } from "./components/LetterUsed"
 
@@ -11,14 +12,10 @@ import styles from "./app.module.css"
 
 export default function App(){
 
-  const [attempts, setattempts] = useState(0)
-  const [letterUsed, setletterUsed] = useState<LetterUsedProps[]>([])
+  const [attempts, setAttempts] = useState(0)
+  const [letterUsed, setLetterUsed] = useState<LetterUsedProps[]>([])
   const [letter, setLetter] = useState("")
   const [challenge, setChallenge] = useState<Challenge | null>(null)
-
-  function handleRestart(){
-    alert("Reiniciar o jogo!")
-  }
 
   function startGame() {
     const index = Math.floor(Math.random() * WORDS.length)
@@ -26,7 +23,42 @@ export default function App(){
 
     setChallenge(randomWord)
 
-    setattempts(0) 
+    setAttempts(0) 
+    setLetter("")
+    setLetterUsed([])
+
+  }
+
+  function handleConfirm(){
+
+    if(!challenge){
+      return
+    }
+
+    if(!letter.trim()) {
+      alert("Digite uma letra!")
+    }
+
+    const value = letter.toUpperCase()
+
+    if(letterUsed.find(used => used.value.toUpperCase() === value)){
+      alert("Você já usou essa letra!")
+      return
+    }
+    
+    const correct = challenge.word.toUpperCase().includes(value)
+
+    if(!correct){
+      setAttempts(attempts + 1)
+    }
+
+    if(attempts + 1 >= 10 ) {
+      alert("Você perdeu, o jogo será reiniciado")
+      startGame()
+    }
+
+    setLetterUsed((prevState) => [...prevState, {value, correct: correct}])
+
     setLetter("")
 
   }
@@ -42,20 +74,29 @@ export default function App(){
   return (
     <div className={styles.container}>
       <main>
-        <Header current={attempts} max={10} onRestart={handleRestart}/>
+        <Header current={attempts} max={10} onRestart={startGame}/>
         
-        <Tip textoDica={"teste"}/>
+        <Tip textoDica={challenge.tip}/>
         
         <div className={styles.word}>
-          {
-            challenge.word.split("").map(() => (
-              <Letter value="" />
-            ))
-          }
+        {
+          challenge.word.split("").map((letter) => {
+            const letterCorrect = letterUsed.some(
+              used => used.value.toUpperCase() === letter.toUpperCase() && used.correct
+            )
 
+            return (
+              <Letter value={letterCorrect ? letter : ""} />
+            )
+          })
+        }
         </div>
 
-        <Palpite />
+        <h4>Palpite</h4>
+        <div className={styles.guess}>
+          <Input autoFocus maxLength={1} placeholder="?" value={letter} onChange= {(event) => setLetter(event.target.value)}/>
+          <Button title="Confirmar" onClick={handleConfirm} />
+        </div>
 
         <div>
           <LetterUsed data={letterUsed}/>
